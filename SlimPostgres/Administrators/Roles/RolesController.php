3b1e76dc6d03d6b1019369dc58218d113c950521
DBCL <greg@it-all.com>
2018-07-03 10:22:56 -0400

    example code for extending validation

--- SlimPostgres/Administrators/Roles/RolesController.php
+++ SlimPostgres/Administrators/Roles/RolesController.php
@@ -6,6 +6,9 @@ namespace SlimPostgres\Administrators\Roles;
 use SlimPostgres\App;
 use SlimPostgres\DatabaseTableController;
 use Slim\Container;
+use Slim\Http\Request;
+use Slim\Http\Response;
+use SlimPostgres\Forms\FormHelper;
 
 class RolesController extends DatabaseTableController
 {
@@ -13,6 +16,33 @@ class RolesController extends DatabaseTableController
     {
         parent::__construct($container, RolesMapper::getInstance(), new RolesView($container), ROUTEPREFIX_ROLES);
     }
+
+    // can override for custom validator
+    // public function postInsert(Request $request, Response $response, $args)
+    // {
+    //     if (!$this->authorization->isFunctionalityAuthorized(App::getRouteName(true, $this->routePrefix, 'insert'))) {
+    //         throw new \Exception('No permission.');
+    //     }
+
+    //     $this->setRequestInput($request, $this->getBooleanFieldNames());
+
+    //     $validator = new RolesValidator($_SESSION[App::SESSION_KEY_REQUEST_INPUT]);
+
+    //     if (!$validator->validate()) {
+    //         // redisplay the form with input values and error(s)
+    //         FormHelper::setFieldErrors($validator->getFirstErrors());
+    //         return $this->view->insertView($request, $response, $args);
+    //     }
+
+    //     try {
+    //         $this->insert();
+    //     } catch (\Exception $e) {
+    //         throw new \Exception("Insert failure. ".$e->getMessage());
+    //     }
+
+    //     FormHelper::unsetFormSessionVars();
+    //     return $response->withRedirect($this->router->pathFor(App::getRouteName(true, $this->routePrefix, 'index')));
+    // }
 
     // override to check condition and add custom return column
     protected function delete($primaryKey, string $returnColumn = null, bool $sendEmail = false)
---
+++ SlimPostgres/Administrators/Roles/RolesValidator.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace SlimPostgres\Administrators\Roles;
+
+use SlimPostgres\DatabaseTableFormValidator;
+
+// can add custom validation rules to roles 
+class RolesValidator extends DatabaseTableFormValidator
+{
+    public function __construct(array $inputData, string $databaseAction = 'insert', array $record = null)
+    {
+        if ($databaseAction != 'insert' && $databaseAction != 'update') {
+            throw new \Exception("databaseAction must be insert or update: $databaseAction");
+        }
+        if ($databaseAction == 'insert' && $record !== null) {
+            throw new \Exception("insert action must not have record");
+        }
+
+        parent::__construct($inputData, RolesMapper::getInstance());
+
+        if ($databaseAction == 'update') {
+            $skipUniqueForUnchanged = true;
+        } else {
+            $skipUniqueForUnchanged = false;
+            $record = []; // override even if entered
+        }
+
+        parent::setRules($skipUniqueForUnchanged, $record);
+
+        // add any custom rules below ie to level (not necessary because already picked up by column type and constraint):
+        // $this->rule('integer', 'level');
+        // $this->rule('min', 'level', 0);
+    }
+}

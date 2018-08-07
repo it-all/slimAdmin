8b4745fba9c19d34cbdee4c34990f7d8cf965298
DBCL <greg@it-all.com>
2018-08-06 22:39:18 -0400

    remove duplicate array value

--- SlimPostgres/App.php
+++ SlimPostgres/App.php
@@ -76,7 +76,7 @@ class App
 
     public function __construct()
     {
-        $this->commonConfigSettingsKeys = ['isLive', 'businessName', 'businessDba', 'administratorDefaultRole', 'emails', 'authentication', 'authorization', 'administratorDefaultRole', 'trimAllUserInput'];
+        $this->commonConfigSettingsKeys = ['isLive', 'businessName', 'businessDba', 'administratorDefaultRole', 'emails', 'authentication', 'authorization', 'trimAllUserInput'];
         $dotenv = new Dotenv(APPLICATION_ROOT_DIRECTORY);
         $dotenv->load();
 

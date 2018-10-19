b08c4303460f1a92613a98b8764002129a98ecd6
Greg Catalano <greg@it-all.com>
2018-10-19 09:25:01 -0400

    remove skeleton folders and files

--- Exceptions/QueryResultsNotFoundException.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace Exceptions;
-
-class QueryResultsNotFoundException extends \RuntimeException 
-{
-    
-}
--- templates/admin/lists/partials/endMain.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-$endMain = <<<EOT
-            </tbody>
-        </table>
-    </div>
-</main>
-EOT;
